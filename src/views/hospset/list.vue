ede394c2a774bb0b92db9b022f61e1fa95dec815
徐濠钦 <1601051004@qq.com>
2022-01-04 17:43:03 +0800

    第一次提交p48 8:01 路由配置

---
+++ src/views/hospset/list.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="app-container">
+    <!-- banner列表 -->
+    <el-table
+      :data="list"
+      stripe
+      style="width: 100%">
+
+      <el-table-column type="index" width="50"/>
+      <el-table-column prop="hosname" label="医院名称"/>
+      <el-table-column prop="hoscode" label="医院编号"/>
+      <el-table-column prop="apiUrl" label="api基础路径" width="200"/>
+      <el-table-column prop="contactsName" label="联系人姓名"/>
+      <el-table-column prop="contactsPhone" label="联系人手机"/>
+      <el-table-column label="状态" width="80">
+        <template slot-scope="scope">
+          {{ scope.row.status === 1 ? '可用' : '不可用' }}
+        </template>
+      </el-table-column>
+    </el-table>
+  </div>
+</template>
