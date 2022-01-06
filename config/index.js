9288015eeb30ca9f134346d58a3ad9c2629ae639
徐濠钦 <1601051004@qq.com>
2022-01-06 17:22:00 +0800

    easyExcel之前，自己敲

--- config/index.js
+++ config/index.js
@@ -13,7 +13,7 @@ module.exports = {
 
     // Various Dev Server settings
     host: 'localhost', // can be overwritten by process.env.HOST
-    port: 8202, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
+    port: 8200, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
     autoOpenBrowser: false,
     errorOverlay: true,
     notifyOnErrors: false,
