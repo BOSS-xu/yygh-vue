24b917eaec8ad7511c51e1226b4a030e935ade65
徐濠钦 <1601051004@qq.com>
2022-01-07 17:02:42 +0800

    nginx之前，p64完

--- src/views/dict/list.vue
+++ src/views/dict/list.vue
@@ -1,6 +1,12 @@
 <template>
   <div
     class="app-container">
+    <div class="el-toolbar">
+      <div class="el-toolbar-body" style="justify-content: flex-start;">
+        <el-button type="text" @click="exportData"><i class="fa fa-plus"/> 导出</el-button>
+        <el-button type="text" @click="importData"><i class="fa fa-plus"/> 导入</el-button>
+      </div>
+    </div>
     <el-table
       :data="list"
       :load="getChildrens"
@@ -44,6 +50,27 @@
         </template>
       </el-table-column>
     </el-table>
+    <el-dialog :visible.sync="dialogImportVisible" title="导入" width="480px">
+      <el-form label-position="right" label-width="170px">
+
+        <el-form-item label="文件">
+          <el-upload
+            :multiple="false"
+            :on-success="onUploadSuccess"
+            :action="'http://localhost:8202/admin/cmn/dict/importData'"
+            class="upload-demo">
+            <el-button size="small" type="primary">点击上传</el-button>
+            <div slot="tip" class="el-upload__tip">只能上传xls文件，且不超过500kb</div>
+          </el-upload>
+        </el-form-item>
+
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="dialogImportVisible = false">
+          取消
+        </el-button>
+      </div>
+    </el-dialog>
   </div>
 </template>
 
@@ -53,7 +80,9 @@ import dict from '@/api/cmn/dict'
 export default {
   data() {
     return {
-      list: [] // 数据字典列表数组
+      list: [], // 数据字典列表数组
+      listLoading: true,
+      dialogImportVisible: false
     }
   },
   created() {
@@ -71,6 +100,18 @@ export default {
       dict.dictList(tree.id).then(response => {
         resolve(response.data)
       })
+    },
+    exportData() {
+      window.location.href = 'http://localhost:8202/admin/cmn/dict/exportData'
+    },
+    importData() {
+      this.dialogImportVisible = true
+    },
+
+    onUploadSuccess(response, file) {
+      this.$message.info('上传成功')
+      this.dialogImportVisible = false
+      this.fetchData()
     }
   }
 }
